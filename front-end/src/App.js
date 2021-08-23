31f04c2450167f2caf08b9e1bca8611f48963150
Amy Lam <amy@Marcos-MBP.hsd1.ca.comcast.net>
2021-08-23 11:26:16 -0700

    Adding nav bar and route to components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Plant from './components/Plant';
+import PlantsList from './components/PlantsList';
+import ProfilePage from './components/ProfilePage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <nav>
+        <div className='nav-links'>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/plants-list">Shop</Link>
+          <Link to="/profile">Profile</Link>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route path="/plants-list/:plantId">
+          <Plant />
+        </Route>
+
+        <Route path="/plants-list">
+          <PlantsList />
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/profile">
+          <ProfilePage />
+        </Route>
+      </Switch>
     </div>
   );
 }

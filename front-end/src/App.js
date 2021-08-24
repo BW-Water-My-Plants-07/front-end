1d5889b038160df92ce4fc32b41d1211977d86df
JackOrth <jackorth19@gmail.com>
2021-08-24 10:58:54 -0600

    completed styling.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,6 +7,57 @@ import PlantsList from './components/PlantsList';
 import ProfilePage from './components/ProfilePage';
 import SignUp from './components/SignUp'
 import AddPlantForm from './components/AddPlantForm'
+import './App.css'
+import styled from 'styled-components'
+
+const StyledApp = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+  height: 100%;
+  width: 100%;
+  
+  .App-header {
+    display: flex;
+    justify-content: space-between;
+    background-color: #9DC88D;
+    height: 100px;
+  }
+  .flex-container-left {
+    display: flex;
+    flex-direction: column;
+    h1 {
+      font-size: 2.5rem;
+      font-weight: bold;
+      margin-top: 30px;
+      color: #164A41;
+      width: 30vw;
+      font-family: 'Cormorant', serif;
+   }
+  }
+  nav {
+    display: flex;
+    justify-content: space-evenly;    
+    align-items: center;
+    width: 500px;
+  }
+  .nav-links {
+    display: flex;
+    justify-content: space-evenly;    
+  }
+  .nav-links a{
+    text-decoration: none;
+    color: #164A41;
+    font-weight: bold;
+    font-size: 1rem;
+    margin-right: 2%;
+    width: 80px;
+    height: 25px;
+    padding-top: 2%;
+  }
+  .nav-links a:hover {
+    color: #fceed1;
+  }
+`
 
 
 function App() {
@@ -14,47 +65,54 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <StyledApp id="wrapper">
+        <header className="App-header">
+          <div className='flex-container-left'>
+            <h1>Water My Plants</h1>
 
-      </header>
-      <nav>
-        <div className='nav-links'>
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/plants-list">Shop</Link>
-          <Link to="/profile">Profile</Link>
-          <Link to="/sign-up">Sign Up</Link>
-          <Link to="add-plant">Add Plant</Link>
-        </div>
-      </nav>
+          </div>
+          <nav>
+            <div className='nav-links'>
+              <Link to="/">Home</Link>
+              <Link to="/login">Login</Link>
+              <Link to="/plants-list">Shop</Link>
+              <Link to="/profile">Profile</Link>
+              <Link to="/add-plant">Add Plant</Link>
+            </div>
+          </nav>
+        </header>
 
-      <Switch>
-        <Route path="/plants-list/:plantId">
-          <Plant items={stock}/>
-        </Route>
+        <Switch>
+          <Route path="/plants-list/:plantId">
+            <Plant items={stock} />
+          </Route>
 
-        <Route path="/plants-list">
-          <PlantsList items={stock}/>
-        </Route>
-        <Route path="/add-plant">
-          <AddPlantForm />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        
-        <Route path="/sign-up">
-          <SignUp />
-        </Route>
+          <Route path="/plants-list">
+            <PlantsList items={stock} />
+          </Route>
 
-        <Route path="/profile">
-          <ProfilePage />
-        </Route>
+          <Route path="/add-plant">
+            <AddPlantForm />
+          </Route>
+          
+          <Route path="/login">
+            <Login />
+          </Route>
 
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+          <Route path="/sign-up">
+            <SignUp />
+          </Route>
+
+          <Route path="/profile">
+            <ProfilePage />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </StyledApp>
+      
     </div>
   );
 }

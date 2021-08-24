7197a624a8fb1cfc9870c58e6ead2c69aa2af2b7
Amy Lam <amy@Marcos-MBP.hsd1.ca.comcast.net>
2021-08-24 09:05:15 -0700

    Applied styling

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,49 +6,114 @@ import Plant from './components/Plant';
 import PlantsList from './components/PlantsList';
 import ProfilePage from './components/ProfilePage';
 import SignUp from './components/SignUp'
+import './App.css'
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+  height: 100%;
+  width: 100%;
+  
+
+  .App-header {
+    display: flex;
+    justify-content: space-between;
+    background-color: #9DC88D;
+    height: 100px;
+  }
+
+  .flex-container-left {
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+      font-size: 2.5rem;
+      font-weight: bold;
+      margin-top: 30px;
+      color: #164A41;
+      width: 30vw;
+      font-family: 'Cormorant', serif;
+   }
+
+  }
+
+  nav {
+    display: flex;
+    justify-content: space-evenly;    
+    align-items: center;
+    width: 500px;
+  }
+
+  .nav-links {
+    display: flex;
+    justify-content: space-evenly;    
+  }
+
+  .nav-links a{
+    text-decoration: none;
+    color: #164A41;
+    font-weight: bold;
+    font-size: 1rem;
+    margin-right: 2%;
+    width: 80px;
+    height: 25px;
+    padding-top: 2%;
+  }
+
+  .nav-links a:hover {
+    color: #fceed1;
+  }
+`
 
 function App() {
   const [stock, setStock] = useState([])
 
   return (
     <div className="App">
-      <header className="App-header">
+      <StyledApp id="wrapper">
+        <header className="App-header">
+          <div className='flex-container-left'>
+            <h1>Water My Plants</h1>
 
-      </header>
-      <nav>
-        <div className='nav-links'>
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/plants-list">Shop</Link>
-          <Link to="/profile">Profile</Link>
-          <Link to="/sign-up">Sign Up</Link>
-        </div>
-      </nav>
+          </div>
+          <nav>
+            <div className='nav-links'>
+              <Link to="/">Home</Link>
+              <Link to="/login">Login</Link>
+              <Link to="/plants-list">Shop</Link>
+              <Link to="/profile">Profile</Link>
+              <Link to="/sign-up">Sign Up</Link>
+            </div>
+          </nav>
+        </header>
 
-      <Switch>
-        <Route path="/plants-list/:plantId">
-          <Plant items={stock}/>
-        </Route>
+        <Switch>
+          <Route path="/plants-list/:plantId">
+            <Plant items={stock} />
+          </Route>
 
-        <Route path="/plants-list">
-          <PlantsList items={stock}/>
-        </Route>
+          <Route path="/plants-list">
+            <PlantsList items={stock} />
+          </Route>
 
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/sign-up">
-          <SignUp />
-        </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/sign-up">
+            <SignUp />
+          </Route>
 
-        <Route path="/profile">
-          <ProfilePage />
-        </Route>
+          <Route path="/profile">
+            <ProfilePage />
+          </Route>
 
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </StyledApp>
+
     </div>
   );
 }
--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,7 +1,76 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledHome = styled.div`
+    .home-wrapper {
+        display: flex;
+        flex-flow: column nowrap;
+        align-items: center;
+    }
+
+    .home-image {
+        width: 100%;
+        max-height: 600px;
+        object-fit: cover;
+    }
+
+    p {
+        margin-top: -350px;
+        width: 350px;
+        font-size: 1.8rem;
+        opacity: 0.6;
+        background-color: #DADED4;
+        padding: 5% 5% 5% 5%;
+        border-radius: 50px;
+        font-family: 'EB Garamond', serif;
+    }
+
+    .btn {
+        background: #1AAB8A;
+        color: #fff;
+        border: none;
+        position: relative;
+        height: 40px;
+        font-size: 1.5em;
+        padding: 0 1.5em;
+        transition: 800ms ease all;
+        outline: none;
+        margin: 10px;
+        border-radius: 10px;
+    }
+
+    .btn:hover {
+        background: #fff;
+        color: #1AAB8A;
+    }
+
+`
 
 export default function Home() {
+    const history = useHistory()
+    const routeToSignUp = () => history.push('/sign-up');
+
     return (
-        <div></div>
+        <StyledHome>
+            <div className='home-wrapper'>
+                <img
+                    className='home-image'
+                    src='https://images.unsplash.com/photo-1592296429945-93008c7e5a59?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+                    alt='Green leaves'
+                />
+                <p>
+                    Never forget when it's time to feed your foliage and quench your
+                    plants' thirst.
+                </p>
+                <button
+                    onClick={routeToSignUp}
+                    className='btn'>
+                        Sign Up
+                </button>
+
+
+            </div>
+        </StyledHome>
     )
 }
